a7d63225300590504ffc39c52f2d81e6bf644d3c
Mircea Davidescu <mdavidescu@protonmail.com>
2020-09-17 11:43:34 +0200

    Created a setup file to install all the required packages.

---
+++ src/setup_file.R
@@ -0,0 +1,22 @@
+# Install standard CRAN packages
+install.packages(c("tidyverse", "forcats", "cowplot", "jpeg", "grid", "alphahull", "modelr", "polynom", "stringr",
+                   "pracma", "sp", "latex2exp", "zoo"))
+
+# Install Bioconductor packages
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("rhdf5")
+
+
+# Install packages developed by Mircea Davidescu on GitHub
+install.packages("devtools")
+library(devtools)
+install_github("MirceaDavidEsc/mover")
+install_github("MirceaDavidEsc/bigh5")
+install_github("MirceaDavidEsc/collective")
+install_github("MirceaDavidEsc/scalefree")
+install_github("MirceaDavidEsc/mover")
+
+# Install multidplyr for parallel processing
+devtools::install_github("tidyverse/multidplyr")
