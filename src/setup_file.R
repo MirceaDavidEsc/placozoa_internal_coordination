94939a186bf52bdeabe94f2c05e8d1af99b095bd
Mircea Davidescu <mdavidescu@protonmail.com>
2020-11-04 10:23:09 +0100

    Split simulation analysis from main analysis. Switched to using Szabo model. Moved setup file outside of mail file.

--- src/setup_file.R
+++ src/setup_file.R
@@ -1,6 +1,6 @@
 # Install standard CRAN packages
 install.packages(c("tidyverse", "forcats", "cowplot", "jpeg", "grid", "alphahull", "modelr", "polynom", "stringr",
-                   "pracma", "sp", "latex2exp", "zoo"))
+                   "pracma", "sp", "latex2exp", "polynom", "zoo"))
 
 # Install Bioconductor packages
 if (!requireNamespace("BiocManager", quietly = TRUE))
@@ -12,10 +12,11 @@ BiocManager::install("rhdf5")
 # Install packages developed by Mircea Davidescu on GitHub
 install.packages("devtools")
 library(devtools)
+
 install_github("MirceaDavidEsc/mover")
 install_github("MirceaDavidEsc/bigh5")
 install_github("MirceaDavidEsc/collective")
-install_github("MirceaDavidEsc/scalefree")
+# install_github("MirceaDavidEsc/scalefree") # BROKEN ON GITHUB? Need to figure out why
 install_github("MirceaDavidEsc/mover")
 
 # Install multidplyr for parallel processing
