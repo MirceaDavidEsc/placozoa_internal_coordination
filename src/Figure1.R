84d924d292b7908cf87022b39dfa3f3771450169
Mircea Davidescu <mdavidescu@protonmail.com>
2021-02-15 16:56:09 -0500

    - Paper revision just prior to submission. Multiple changes to figure 1 and 3 for the February 2021 version of the manuscript. Important changes to the scale free sim analysis were also complete.

--- src/Figure1.R
+++ src/Figure1.R
@@ -10,75 +10,70 @@ imgRaster1 = rasterGrob(frame1)
 imgRaster2 = rasterGrob(frame2)
 imgRaster3 = rasterGrob(frame3)
 
-(frame1Plot = ggplot(conversionFactors, aes()) + annotation_custom(imgRaster1) + coord_fixed(ratio=1))
-frame2Plot = ggplot(conversionFactors, aes()) + annotation_custom(imgRaster2) + coord_fixed(ratio=1)
-frame3Plot = ggplot(conversionFactors, aes()) + annotation_custom(imgRaster3) + coord_fixed(ratio=1)
+(frame1Plot = ggplot(conversionFactors, aes()) + annotation_custom(imgRaster1) + coord_fixed(ratio=1) + theme_minimal())
+(frame2Plot = ggplot(conversionFactors, aes()) + annotation_custom(imgRaster2) + coord_fixed(ratio=1) + theme_minimal())
+(frame3Plot = ggplot(conversionFactors, aes()) + annotation_custom(imgRaster3) + coord_fixed(ratio=1) + theme_minimal())
 
-(snapshotsPlot = plot_grid(frame1Plot, frame2Plot, frame3Plot, ncol = 3, labels = c("(i)", "(ii)", "(iii)"), scale = 2))
+(snapshotsPlot = plot_grid(frame1Plot, frame2Plot, frame3Plot, ncol = 3, scale = 2))
+
+
+plot_animal_velocity_field = function(frameData, arrows = F, arrows_round = 5, arrowl = 1, scalebar = F, scalebar_l = 100, scalebar_w = 3) {
+  colnames(frameData) = c("X", "Y", "vX", "vY", "speed", "angle")
+  
+  segmentData = frameData %>% mutate(Xround = round(X/arrows_round) * arrows_round, Yround = round(Y/arrows_round) * arrows_round) %>%
+    mutate(outBounds = Xround^2 + Yround^2 > X^2 + Y^2) %>%
+    group_by(Xround, Yround) %>% summarise(meanVX = mean(vX * arrowl), meanVY = mean(vY * arrowl), allOut = min(outBounds)) %>%
+    filter(!allOut)
+  
+  (return_plot = ggplot(frameData) + geom_tile(aes(X, Y, fill=angle)) +
+    scale_fill_gradientn(name = expression(theta), colours = cbPalette, limits = c(-pi,pi), breaks=c(-pi, 0, pi)) +
+    theme_classic() +
+    theme(axis.line = element_blank(), axis.ticks = element_blank(), axis.text = element_blank(), axis.title = element_blank()) + 
+    coord_fixed() + guides(fill = F))
+    
+  if (arrows) {
+    return_plot = return_plot + geom_segment(data = segmentData, aes(Xround, Yround, xend = Xround + meanVX * arrowl, yend = Yround + meanVY * arrowl), 
+                                              size = 1, color = "black", alpha = 1, arrow=arrow(angle=20,length=unit(0.4,"cm")))  
+  }
+  
+  if (scalebar) {
+    return_plot = return_plot + annotate("rect", alpha = 1, fill = "black", xmin = max(frameData$X) - scalebar_l, xmax = max(frameData$X), ymin = min(frameData$Y), ymax = min(frameData$Y) + scalebar_w)
+  }
+  
+  return(return_plot)
+}
+
 
 
 
 # Make the velocity field plots.
-frameData = read_csv(paste(projectFolder, "Data_Processed", "polarizedFrame.csv", sep = "/"))
-frameDataArrows = frameData %>% filter(X %% 10 == 0 & Y %% 10 == 0) %>% mutate(X = X - mean(frameData$X), Y = Y - mean(frameData$Y))
-frameData = mutate(frameData, X = X - mean(X), Y = Y - mean(Y))
-
-zoom = 36
-
-(arrow_plot_small_polarized = ggplot(frameData) + geom_tile(aes(X, Y, fill=angle, alpha=speed)) +
-    scale_fill_gradientn(name = expression(theta), colours = cbPalette, limits = c(-pi,pi), breaks=c(-pi, 0, pi)) +
-    geom_segment(data = data.frame(XStart = min(frameData$X), XEnd = min(frameData$X) + 47, YStart = min(frameData$Y) - 5, YEnd = min(frameData$Y) - 5), 
-                 aes(XStart, YStart, xend = XEnd, yend = YEnd), size = 3) + xlim(-zoom,zoom) + ylim(-zoom,zoom) + coord_fixed() + 
-    guides(alpha = F, fill = F) + theme(axis.line = element_blank(), axis.ticks = element_blank(), axis.text = element_blank(), axis.title = element_blank()) +
-    geom_segment(data = frameDataArrows, aes(X, Y, xend = X+vX, yend = Y+vY), size = 1, color = "black", alpha = 1,
-                 arrow=arrow(angle=20,length=unit(0.4,"cm"))))
-
-frameData = read_csv(paste(projectFolder, "Figures", "Small_Rotation.csv", sep = "/"))
-## Center the data
-frameDataArrows = frameData %>% filter(X %% 10 == 0 & Y %% 10 == 0)
-frameDataArrows = mutate(frameDataArrows, X = X - mean(frameData$X), Y = Y - mean(frameData$Y))
-frameData = mutate(frameData, X = X - mean(X), Y = Y - mean(Y))
-
-(arrow_plot_small_rotating = ggplot(frameData) + geom_tile(aes(X, Y, fill=angle, alpha=speed)) +
-    scale_fill_gradientn(name = expression(theta), colours = cbPalette, limits = c(-pi,pi), breaks=c(-pi, 0, pi),
-                         labels=c(expression(paste("-",pi,sep="")), 0, expression(paste(pi)))) +
-    xlim(-zoom,zoom) + ylim(-zoom,zoom) + coord_fixed() + 
-    guides(alpha = F, fill = F) + theme(axis.line = element_blank(), axis.ticks = element_blank(), axis.text = element_blank(), axis.title = element_blank()) +
-    geom_segment(data = frameDataArrows, aes(X, Y, xend = X+vX, yend = Y+vY), size = 1, color = "black", alpha = 1,
-                 arrow=arrow(angle=20,length=unit(0.4,"cm"))))
+polarizedSmall = read_csv(paste(projectFolder, "Data_Processed", "polarizedFrame.csv", sep = "/"))
+polarizedSmall = mutate(polarizedSmall, X = X - mean(X), Y = Y - mean(Y))
+(arrow_plot_small_polarized = plot_animal_velocity_field(polarizedSmall, arrows = T, arrows_round = 10, arrowl = 1, scalebar = T, scalebar_l = 30, scalebar_w = 2))
 
 
-polarizedFrame = read_csv(paste(projectFolder, "Figures", "Big_Polarized.csv", sep = "/"))
-frameData = polarizedFrame %>% mutate(speed = sqrt(vX^2 + vY^2), angle = atan2(x = vX, y = vY))
-frameDataArrows = frameData %>% filter(X %% 10 == 0 & Y %% 10 == 0) %>%
-  mutate(X = X - mean(frameData$X), Y = Y - mean(frameData$Y))
-frameData = frameData %>% mutate(X = X - mean(X), Y = Y - mean(Y))
+rotatingSmall = read_csv(paste(projectFolder, "Figures", "Small_Rotation.csv", sep = "/"))
+rotatingSmall = mutate(rotatingSmall, X = X - mean(X), Y = Y - mean(Y))
+
+(arrow_plot_small_rotating = plot_animal_velocity_field(rotatingSmall, arrows = T, arrows_round = 10, arrowl = 1))
+
+
+
+polarizedBig = read_csv(paste(projectFolder, "Figures", "Big_Polarized.csv", sep = "/")) %>% 
+  mutate(speed = sqrt(vX^2 + vY^2), angle = atan2(x = vX, y = vY)) %>% 
+  mutate(X = X - mean(X), Y = Y - mean(Y))
 
 ## Define boundaries of image, based on min and max of frame.
-boundary = max(abs(c(range(frameData$X), range(frameData$Y))))
 
-(arrow_plot_big_order = ggplot(frameData) + geom_tile(aes(X, Y, fill=angle, alpha=speed)) +
-    scale_fill_gradientn(name = expression(theta), colours = cbPalette, limits = c(-pi,pi), breaks=c(-pi, 0, pi)) + coord_fixed() + 
-    guides(alpha = F, fill = F) + theme(axis.line = element_blank(), axis.ticks = element_blank(), axis.text = element_blank(), axis.title = element_blank()) +
-    geom_segment(data = frameDataArrows, aes(X, Y, xend = X+vX, yend = Y+vY), size = 1, color = "black", alpha = 1,
-                 arrow=arrow(angle=20,length=unit(0.4,"cm"))) +
-    geom_segment(data = data.frame(XStart = min(frameData$X), XEnd = min(frameData$X) + 47, YStart = min(frameData$Y) - 5, YEnd = min(frameData$Y) - 5), 
-                 aes(XStart, YStart, xend = XEnd, yend = YEnd), size = 3) +
-    xlim(-boundary, boundary) + ylim(-boundary,boundary))
+(arrow_plot_big_order = plot_animal_velocity_field(polarizedBig, arrows = T, arrows_round = 30, arrowl = 1.5, scalebar = T, scalebar_l = 30, scalebar_w = 10))
 
 
-frameData = read_csv(paste(projectFolder, "Data_Processed", "Big_Disorganzed.csv", sep = "/"))
-frameDataArrows = frameData %>% filter(X %% 10 == 0 & Y %% 10 == 0)
-frameDataArrows = frameDataArrows %>% mutate(X = X - mean(frameData$X), Y = Y - mean(frameData$Y))
-frameData = frameData %>% mutate(X = X - mean(X), Y = Y - mean(Y))
-(arrow_plot_big_disorder = ggplot(frameData) + geom_tile(aes(X, Y, fill=angle, alpha=speed)) +
-    scale_fill_gradientn(name = expression(theta), colours = cbPalette, limits = c(-pi,pi), breaks=c(-pi, 0, pi))  + coord_fixed() + 
-    guides(alpha = F, fill = F) + theme(axis.line = element_blank(), axis.ticks = element_blank(), axis.text = element_blank(), axis.title = element_blank()) +
-    geom_segment(data = frameDataArrows, aes(X, Y, xend = X+vX, yend = Y+vY), size = 1, color = "black", alpha = 1,
-                 arrow=arrow(angle=20,length=unit(0.4,"cm"))) +
-    xlim(-boundary, boundary) + ylim(-boundary,boundary))
+disorganizedBig = read_csv(paste(projectFolder, "Data_Processed", "Big_Disorganzed.csv", sep = "/")) %>% 
+  mutate(X = X - mean(X), Y = Y - mean(Y))
 
-(velocitySnaps = plot_grid(arrow_plot_small_polarized, arrow_plot_small_rotating, arrow_plot_big_order, arrow_plot_big_disorder, ncol = 2, labels = c("B", "C", "D", "E"), scale = 1, label_size = 32))
+(arrow_plot_big_disorder = plot_animal_velocity_field(disorganizedBig, arrows = T, arrows_round = 30, arrowl = 1.5))
+
+(velocitySnaps = plot_grid(arrow_plot_small_polarized, arrow_plot_small_rotating, arrow_plot_big_order, arrow_plot_big_disorder, ncol = 2, labels = c("B", "C", "D", "E"), scale = 1, label_size = 20))
 
 # Order vs size plot
 relevantOrderData = read_rds(paste(projectFolder, processedDataFolder, "relevantOrderData.rds", sep = "/"))
@@ -88,11 +83,10 @@ uncurledCount = relevantOrderData %>% group_by(folder) %>% summarise(count=n())
 uncurledOrderSummary = inner_join(uncurledOrderSummary, uncurledCount)
 
 (collectiveOrderSizePlot = ggplot(uncurledOrderSummary, aes(EquivalentDiameter_mean, crystalMeasure_mean)) + geom_point() + 
-    xlab(expression(paste("D (",mu,"m)",sep=""))) + 
-    scale_y_continuous("Order", breaks = c(0.7, 0.8, 0.9)) + stat_smooth(method="lm"))
+    xlab(expression(paste("Diameter (",mu,"m)",sep=""))) + 
+    scale_y_continuous("Order", breaks = c(0.7, 0.8, 0.9)) + stat_smooth(method="lm") +
+    theme_classic())
+collectiveOrderSizePlot = increase_text_size(collectiveOrderSizePlot)
 
-# Use if you want order plot side-by-side with snapshots.
-# (bottomHalf = plot_grid(velocitySnaps, collectiveOrderSizePlot, labels = c("", "F"), label_size = 32))
-
-fullFigure = plot_grid(snapshotsPlot, velocitySnaps, collectiveOrderSizePlot, ncol = 1, rel_heights = c(0.4, 0.4, 0.2), labels = c("A", "", "F"), label_size = 32)
-save_plot(filename = paste(projectFolder, "Figures/Figure1.pdf", sep = ""), fullFigure, base_width = 6, base_height = 12)
+(fullFigure = plot_grid(snapshotsPlot, velocitySnaps, collectiveOrderSizePlot, ncol = 1, rel_heights = c(0.4, 0.3, 0.3), labels = c("A", "", "F"), label_size = 20))
+save_plot(filename = paste(projectFolder, "figures/Figure1.pdf", sep = "/"), fullFigure, base_width = 6, base_height = 12)
--- src/Figure3.R
+++ src/Figure3.R
@@ -1,93 +1,149 @@
 # Make figure 3
+slope_regression_plot = function(slopesData) {
+  diameterSlopePlot = ggplot(modelSlopes, aes(EquivalentDiameter, slope)) + geom_point() + 
+    # geom_line(aes(EquivalentDiameter, linear), size = 1, alpha = 0.5, color = "red") + 
+    geom_line(aes(EquivalentDiameter, exponential), size = 1) +
+    xlab(expression(paste("Diameter (",mu,"m)",sep=""))) + 
+    ylab(expression(paste("O'(",eta,"*)", sep = ""))) + 
+    guides(color = F) + theme_classic()
+  return(diameterSlopePlot)
+}
+
+
+noise_order_slopes_plot = function(noiseOrderData, grouping) {
+  ggplot(noiseOrderData, aes(energyRatio, order, color = EquivalentDiameter)) + 
+    geom_point(alpha = 0.2, color = "black") +
+    stat_smooth(aes_string(group = grouping), method = "lm", se = F) + 
+    scale_color_gradient(expression(paste("Diameter (",mu,"m)", sep = ""))) +
+    scale_x_continuous(expression(paste(eta,"*",sep="")), breaks = c(0,0.5,1), limits = c(0,1)) +
+    scale_y_continuous("Order", breaks = c(0,0.5,1), limits = c(0,1)) + guides(color = F) +
+    theme_classic()
+}
+
 
 ## Critical slope - real animal
-modelSlopes = read_rds(paste(projectFolder, "modelSlopes_realdata.rds", sep = ""))
-diameterSlopePlot = ggplot(modelSlopes, aes(EquivalentDiameter, slope)) + geom_point() + 
-  geom_line(aes(EquivalentDiameter, linear), size = 1, alpha = 0.5, color = "red") + geom_line(aes(EquivalentDiameter, exponential), size = 1) +
-  xlab(expression(paste("D (",mu,"m)",sep=""))) + ylab(expression(paste("O'(",eta,"*)", sep = ""))) + guides(color = F) +
-  theme(axis.title = element_text(size= 24), axis.text = element_text(size = 20))
+modelSlopes = read_rds(paste(projectFolder, "modelSlopes_realdata.rds", sep = "/"))
+(modelSlopesPlot = slope_regression_plot(modelSlopes) %>% increase_text_size())
 
-energyOrderSize = read_rds(paste(projectFolder, "energyOrderSize.rds", sep = ""))
-slopesPlotRatio = energyOrderSize %>% filter(complete.cases(.)) %>% ggplot(aes(energyRatio, order, group = folder, color = EquivalentDiameter)) + 
-  geom_point(alpha = 0.2, color = "black") + stat_smooth(method = "lm", se = F) + 
-  scale_color_gradient(expression(paste("D (",mu,"m)", sep = "")), high = "blue", low = "red") +
-  scale_x_continuous(expression(paste(eta,"*",sep="")), breaks = c(0,0.5,1), limits = c(0,1)) +
-  scale_y_continuous("Order", breaks = c(0,0.5,1), limits = c(0,1)) + guides(color = F) +
-  theme_bw() + theme(axis.title = element_text(size= 20), axis.text = element_text(size = 18))
+energyOrderSize_actual = read_rds(paste(projectFolder, "energyOrderSize.rds", sep = "/")) %>% filter(complete.cases(.))
+(slopesPlotRatio = energyOrderSize_actual %>% noise_order_slopes_plot(grouping = "folder") %>% increase_text_size())
 
-(panel2B = ggdraw() + draw_plot(diameterSlopePlot) + draw_plot(slopesPlotRatio, 0.5, 0.5, 0.5, 0.5))
+(diameterSlopes_real = ggdraw() + draw_plot(modelSlopesPlot) + draw_plot(slopesPlotRatio, 0.5, 0.5, 0.5, 0.5))
 
 ## critical slope, simulation
-criticalRange = read_rds(paste(projectFolder, "3C_NoiseOrderRelation_strength.rds", sep = ""))
-(criticalSlopesPlot = ggplot(criticalRange, aes(energyRatio, polarization, group = Size)) + geom_point(color = "black", alpha = 0.2) + 
-    stat_smooth(aes(color = Size), method = "lm", se = F) + scale_color_gradient(high = "blue", low = "red") + 
-    scale_x_continuous(expression(paste(eta,"*",sep="")), breaks = c(0,0.5,1), limits = c(0,1)) + scale_y_continuous("Order", breaks = c(0,0.5,1), limits = c(0,1)) + 
-    scale_linetype_discrete(TeX("\\sqrt{N}")) + guides(color = F))
+energyOrderSize_sim = read_rds(paste(projectFolder, "3C_NoiseOrderRelation_strength.rds", sep = "/")) %>%
+  rename(order = Order) %>% mutate(EquivalentDiameter = sqrt(Size))
+(slopesPlotRatio_sim = energyOrderSize_sim %>% noise_order_slopes_plot(grouping = "Size") %>% increase_text_size())
 
+modelSlopes = read_rds(paste(projectFolder, "3D_NoiseSlope_strength.rds", sep = "/")) %>%
+  mutate(EquivalentDiameter = sqrt(Size))
+modelSlopesPlot_sim = (slope_regression_plot(modelSlopes) + xlab(TeX("Size (\\sqrt{N})"))) %>% increase_text_size()
 
-modelSlopes = read_rds(paste(projectFolder, "3D_NoiseSlope_strength.rds", sep = ""))
-(diameterSlopePlot_sim = ggplot(modelSlopes, aes(Diam, slope)) + geom_point(alpha = 0.5) + xlab(TeX("Size (\\sqrt{N})")) +
-    ylab("S'(r)") + geom_line(aes(Diam, linear), size = 1, alpha = 0.5, color = "red") + 
-    geom_line(aes(Diam, exponential), size = 1))
-
-(panel2B_sim = ggdraw() + draw_plot(diameterSlopePlot_sim) + draw_plot(criticalSlopesPlot, 0.5, 0.5, 0.5, 0.5))
+(diameterSlopes_sim = ggdraw() + draw_plot(modelSlopesPlot_sim) + draw_plot(slopesPlotRatio_sim, 0.55, 0.55, 0.45, 0.45))
 
 
 #################
 
 ## Scale free correlations - real animal
-meanSizes = read_rds(paste(projectFolder, "Data_Processed", "meanSizes.rds", sep = "/"))
-meanProfiles = read_rds(paste(projectFolder, "meanProfiles.rds", sep = "/"))
-meanProfilesStats = read_csv(paste(projectFolder, "Data_Processed", "meanProfileStats.csv", sep = "/"))
-perFrameAvgCorrStats = read_csv("perFrameAvgCorrStats.csv") %>% mutate(Area = pi*(EquivalentDiameter_mean/2)^2)
+meanSizes = read_rds(paste(projectFolder, "Data_Processed", "meanSizes.rds", sep = "/")) %>% ungroup()
+meanProfiles = read_rds(paste(projectFolder, "meanProfiles.rds", sep = "/")) %>% ungroup()
+meanProfilesStats = read_csv(paste(projectFolder, "meanProfileStats.csv", sep = "/")) %>% ungroup()
+perFrameAvgCorrStats = read_csv("perFrameAvgCorrStats.csv") %>% mutate(Area = pi*(EquivalentDiameter_mean/2)^2) %>% ungroup()
 
 profilesWithCrossing = inner_join(meanProfiles, meanProfilesStats) %>% mutate(rescaledDist = domain/vZero) %>% 
   filter(rescaledDist <= 2 & vCorr_mean > -1)
 
-(originalVelocityProfiles = profilesWithCrossing %>% ggplot(aes(domain, vCorr_mean, group=folder, color=EquivalentDiameter)) + geom_line() + 
-    geom_hline(yintercept=0, linetype=2) + scale_x_continuous(expression(paste("D (",mu,"m)")), breaks = c(200,1000)) + 
+profiles_plot = function(profilesDF) {
+   ggplot(profilesDF, aes(domain, vCorr_mean, group=folder, color=EquivalentDiameter)) + geom_line() + 
+    geom_hline(yintercept=0, linetype=2) + scale_x_continuous(expression(paste("Diameter (",mu,"m)")), breaks = c(200,1000)) + 
     scale_y_continuous(name = expression(paste(C[V],"(x)", sep="")), breaks = c(0,0.5,1)) + 
-    scale_color_gradient(name = expression(paste("Size (",mu,"m)",sep="")), limits = c(200,1600), breaks = c(200,900,1600)))
+    scale_color_gradient(name = expression(paste("Size (",mu,"m)",sep="")), limits = c(200,1600), breaks = c(200,900,1600)) +
+    theme_classic()
+}
 
+(originalVelocityProfiles = profiles_plot(profilesWithCrossing))
 
-(vZeroPlot = ggplot(perFrameAvgCorrStats, aes(EquivalentDiameter_mean, vZero_mean)) + geom_point(size=2) + 
+originalVelocityProfiles = originalVelocityProfiles %>% increase_text_size()
+
+corrLength_plot = function()
+
+(vZeroPlot = ggplot(perFrameAvgCorrStats, aes(EquivalentDiameter_mean, vZero_mean)) + geom_point(size=2) + theme_classic() +
     geom_errorbar(aes(ymax=vZero_mean + vZero_sd / sqrt(numFrames), ymin=vZero_mean - vZero_sd / sqrt(numFrames))) +
-    geom_errorbarh(aes(xmax=EquivalentDiameter_mean + EquivalentDiameter_sd / sqrt(numFrames), xmin=EquivalentDiameter_mean-EquivalentDiameter_sd / sqrt(numFrames))) + stat_smooth(method="lm", formula = y ~ -1+x, se = F,size=1,alpha=0.3, color="blue") + 
-    scale_x_continuous(expression(paste("D (",mu,"m)",sep="")), breaks= c(200, 900, 1600)) + ylab(expression(paste(phi[V]," (",mu,"m)",sep=""))))
+    geom_errorbarh(aes(xmax=EquivalentDiameter_mean + EquivalentDiameter_sd / sqrt(numFrames), 
+                       xmin=EquivalentDiameter_mean-EquivalentDiameter_sd / sqrt(numFrames))) + 
+   stat_smooth(method="lm", color = "black", formula = y ~ -1+x, se = F,size=1,alpha=0.3, color="blue") + 
+    scale_x_continuous(expression(paste("Diameter (",mu,"m)",sep="")), breaks= c(200, 900, 1600)) + ylab(expression(paste(phi[V]," (",mu,"m)",sep=""))))
 
-(panel3B_sim = ggdraw() + draw_plot(vZeroPlot) + draw_plot(originalVelocityProfiles + guides(color = F), 0.1, 0.6, 0.4, 0.4))
+vZeroPlot = vZeroPlot %>% increase_text_size()
+
+(diamCorrLengthPlot_wprofiles = ggdraw() + draw_plot(vZeroPlot) + draw_plot(originalVelocityProfiles + guides(color = F), 0.15, 0.65, 0.35, 0.35))
 
 ## Scale free correlations - simulation
-perFrameAvgCorrStats_sim = read_rds(paste(projectFolder, "perFrameAvgStats_sim.rds", sep = ""))
+perFrameAvgCorrStats_sim = read_rds(paste(projectFolder, "perFrameAvgStats_sim.rds", sep = "/")) %>% ungroup()
 
-(vZeroPlot = filter(perFrameAvgCorrStats_sim, Noise %in% c(3.7, 2.0, 5.0)) %>% ggplot(aes(sqrt(Size), vZero_mean, color = as.factor(Noise), group = as.factor(Noise))) + 
-    geom_point(size=2) + geom_line(size = 1) + xlab(TeX("Size (\\sqrt{N})")) + ylab(expression(paste(phi[V]," (a.u.)",sep=""))) + 
-    scale_color_manual(expression(mu), values = c("skyblue", "black", "green")))
+(vZeroPlot_sim = perFrameAvgCorrStats_sim %>% #filter(, Noise %in% c(3.7, 2.0, 5.0)) %>% 
+    ggplot(aes(sqrt(Size), vZero_mean, color = Noise, group = as.factor(Noise))) + theme_classic() +
+    geom_point(size=2) + geom_line(size = 1) + 
+    geom_line(data = filter(perFrameAvgCorrStats_sim, Noise == 3.5), color = "black", size = 2, linetype = 2) +
+    xlab(TeX("Size (\\sqrt{N})")) + ylab(expression(paste(phi[V]," (a.u.)",sep=""))) + 
+    scale_color_gradient("k", high = "blue", low = "red", breaks = seq(2,8, by = 3)))
 
 
 #################
 
 ## Correlation integral - real animal
 
-(rescaledProfilesPlot = profilesWithCrossing %>% ggplot(aes(rescaledDist, vCorr_mean, group=folder, color=EquivalentDiameter)) + geom_line() +  geom_hline(yintercept = 0, linetype=2) + ylab("K(x)") +
+(rescaledProfilesPlot = ggplot(profilesWithCrossing, aes(rescaledDist, vCorr_mean, group=folder, color=EquivalentDiameter)) + 
+   theme_classic() +  geom_line() + 
+   geom_hline(yintercept = 0, linetype=2) + ylab("K(x)") +
    scale_x_continuous(name = expression(paste("x/",phi,sep="")), breaks = c(0, 1.0, 2.0)) +
-   scale_color_gradient(name = expression(paste("D (",mu,"m)",sep="")), limits = c(200,1600), breaks = c(200,900,1600)) + theme(legend.position = c(0.9, 0.8)))
+   scale_color_gradient(name = expression(paste("Diameter (",mu,"m)",sep="")), limits = c(200,1600), breaks = c(200,900,1600)) + 
+   theme(legend.position = c(0.9, 0.8)))
 
-(susceptibilityRelation = ggplot(susceptibilityValues, aes(EquivalentDiameter_mean, vSuscept_mean)) + geom_point() +
-    geom_line(aes(EquivalentDiameter_mean, expFit), size = 1) + 
+rescaledProfilesPlot = rescaledProfilesPlot %>% increase_text_size()
+
+expFit = glm(vSuscept_mean ~ log(EquivalentDiameter_mean), data = perFrameAvgCorrStats)
+perFrameAvgCorrStats = broom::augment_columns(expFit, perFrameAvgCorrStats)
+
+
+(susceptibilityRelation = ggplot(perFrameAvgCorrStats, aes(EquivalentDiameter_mean, vSuscept_mean)) + geom_point() +
+    geom_line(aes(EquivalentDiameter_mean, .fitted), size = 1) + theme_classic() +
     # geom_line(aes(Area, linearFit), size = 1, alpha = 0.5, color = "red") + # No need for linear relationship
-    scale_x_continuous(TeX(paste("D ($\\mu m$)",sep="")), breaks = c(200,900,1600)) +
+    scale_x_continuous(TeX(paste("Diameter ($\\mu m$)",sep="")), breaks = c(200,900,1600)) +
     scale_y_continuous(expression(chi)))
+susceptibilityRelation = susceptibilityRelation %>% increase_text_size()
 
-(panel4B_sim = ggdraw() + draw_plot(susceptibilityRelation) + draw_plot(rescaledProfilesPlot + guides(color = F), 0.1, 0.6, 0.4, 0.4))
+
+(diameterSuscept_wprofiles = ggdraw() + draw_plot(susceptibilityRelation) + 
+    draw_plot(rescaledProfilesPlot + guides(color = F), 0.15, 0.65, 0.4, 0.35))
 
 
 ## correlation integral - simulation
 
-(vSusceptPlot = filter(perFrameAvgCorrStats_sim, Noise %in% c(3.7, 2.0, 5.0)) %>% ggplot(aes(sqrt(Size), vSuscept_mean, color = as.factor(Noise), group = as.factor(Noise))) + 
+(vSusceptPlot = perFrameAvgCorrStats_sim %>% # filter(Noise %in% c(3.7, 2.0, 5.0)) %>% 
+    ggplot(aes(sqrt(Size), vSuscept_mean, color = Noise, group = as.factor(Noise))) + theme_classic() +
     geom_point(size=2) + geom_line(size = 1) +
+    geom_line(data = filter(perFrameAvgCorrStats_sim, Noise == 3.5), color = "black", size = 2, linetype = 2) +
     xlab(TeX("Size (\\sqrt{N})")) + ylab(expression(paste(chi[V]," (a.u.)",sep=""))) + 
-    scale_color_manual(expression(mu), values = c("skyblue", "black", "green")))
+    scale_color_gradient("k", high = "blue", low = "red", breaks = seq(2,8, by = 3)))
 
-fillFigure = plot_grid(panel2B, panel3B_sim, panel4B_sim, panel2B_sim, vZeroPlot, vSusceptPlot, ncol = 3, labels = "AUTO", label_size = 32)
-save_plot(fillFigure, filename = paste(projectFolder, "Figures/Figure3.pdf", sep = ""), base_width = 18, base_height = 10)
+
+
+fullFigure = plot_grid(diameterSlopes_real, 
+                       diamCorrLengthPlot_wprofiles, 
+                       diameterSuscept_wprofiles, 
+                       diameterSlopes_sim, 
+                       increase_text_size(vZeroPlot_sim), 
+                       increase_text_size(vSusceptPlot), ncol = 3, labels = "AUTO", label_size = 32)
+save_plot(fullFigure, filename = paste(projectFolder, "Figures/Figure3.pdf", sep = "/"), base_width = 18, base_height = 10)
+
+
+## Debug calculations
+perFrameAvgCorrStats %>% arrange(desc(vSuscept_mean)) %>% slice(-1) %>%
+  summarise(normalizedDiam = max(EquivalentDiameter_mean) / min(EquivalentDiameter_mean), 
+            norm_vZ = max(vZero_mean) / min(vZero_mean),
+            norm_vS = max(vSuscept_mean) / min(vSuscept_mean))
+
+perFrameAvgCorrStats_sim %>% filter(Size != min(Size)) %>% group_by(Noise) %>% summarise(sizeScale = sqrt(max(Size) / min(Size)), 
+                                                           susceptScale = max(vSuscept_mean) / min(vSuscept_mean),
+                                                           vZeroScale = max(vZero_mean) / min(vZero_mean))
