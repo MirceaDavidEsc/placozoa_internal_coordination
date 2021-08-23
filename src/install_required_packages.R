361a887d3ffe5590b2e1c5a1e98430f4f6f72f97
Mircea Davidescu <mdavidescu@protonmail.com>
2021-08-23 13:03:10 +0200

    Added package installation script

---
+++ src/install_required_packages.R
@@ -0,0 +1,24 @@
+install.packages(c("tidyverse",
+  "forcats",
+  "rhdf5",
+  "cowplot",
+  "jpeg",
+  "grid",
+  "alphahull",
+  "modelr",
+  "polynom",
+  "stringr",
+  "pracma",
+  "sp",
+  "latex2exp",
+  "zoo",
+  "bigsplines",
+  "png"))
+
+install.packages("devtools")
+library(devtools)
+
+install_github("MirceaDavidEsc/bigh5")
+install_github("MirceaDavidEsc/Collective")
+install_github("MirceaDavidEsc/scalefree")
+install_github("MirceaDavidEsc/mover")
